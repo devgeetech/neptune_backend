e5c18ac40df8315a91e6720d9c787e48b771ad19
devgeetech <devgeetech@gmail.com>
2021-05-17 16:07:31 +0530

    req fix, complete integration

--- neptune_prod.py
+++ neptune_prod.py
@@ -33,7 +33,7 @@ def auth():
 
 def create_url():
     query = "earthquake OR cyclone -is:retweet"
-    tweet_fields = "tweet.fields=author_id,created_at,geo&max_results=10"
+    tweet_fields = "tweet.fields=author_id,created_at&max_results=100"
     url = "https://api.twitter.com/2/tweets/search/recent?query={}&{}".format(
         query, tweet_fields
     )
@@ -56,8 +56,8 @@ def connect_to_endpoint(url, headers):
 bearer_token = 'AAAAAAAAAAAAAAAAAAAAAGYZOAEAAAAAD%2FG4jnISJ1wxC%2BdpB9gCjBHnxVM%3DZA8S9wRWY7AJdgQJ3lMCsaf05uhtTHxRtWDY6bIYx9hkB4nB4S'
 url = create_url()
 headers = create_headers(bearer_token)
-i=0;
-while i<10:
+loop_i=0;
+while loop_i<10:
      # INITIALISING VARIABLES
      help_tweets = {}
      location_dictionary = {}
@@ -68,31 +68,43 @@ while i<10:
      dset = json_normalize(json_data['data'])
      # CONSTRUCTING TWEET ONLY DATAFRAME
      newdset = dset.copy(deep=True)
-     newdset.drop(['author_id','created_at','id','geo.coordinates.coordinates', 'geo.coordinates.type', 'geo.place_id' ], axis=1, inplace=True)
+     newdset.drop(['author_id','created_at','id'], axis=1, inplace=True)
      
      # Cleaning Tweets
      def cleanTxt(text):
           text = re.sub('@[A-Za-z0–9]+', '', text) #Removing @mentions     
           text = re.sub('RT[\s]+', '', text) # Removing RT
-          text = re.sub('#', '', text) # Removing RT
           text = re.sub('https?:\/\/\S+', '', text) # Removing hyperlink
+          text = re.sub('#', '', text) # Removing RT
+          text = re.sub('-', '', text) # Removing symbol
+          text = re.sub(',', '', text) # Removing symbol
+          text = re.sub('\.', '', text) # Removing symbol
+          text = re.sub('\(', '', text) # Removing symbol
+          text = re.sub('\)', '', text) # Removing symbol
+          text = re.sub(':', '', text) # Removing colon
+          text = re.sub(';', '', text) # Removing semicolon
+          text = re.sub('\"', '', text) # Removing backslash
+          text = re.sub('/', '', text) # Removing slash
+          text = re.sub('_', '', text) # Removing underscore
           return text
      newdset['text'] = newdset['text'].apply(cleanTxt)
      
      # Removing Stop Words
      all_stopwords = nlp.Defaults.stop_words
      tokenizer = nlp.tokenizer
-     nlp.Defaults.stop_words |= {"\"",";","-","?",".","I","!",":","...","omfg","wtf","omg","sismo"} # updating stop word list
+     nlp.Defaults.stop_words |= {"?","I","!","...","omfg","wtf","omg","sismo","lol"} # updating stop word list
      for index, row in newdset.iterrows():
        tweet_as_tokens = tokenizer(newdset.loc[index].text.lower())
        tokens_without_sw= [token.text for token in tweet_as_tokens if not token.text in all_stopwords]
-       newdset.loc[index].tweet = ' '.join(tokens_without_sw)
+       newdset.loc[index].text = ' '.join(tokens_without_sw)
      
      # High-frequency words:
      most_important_topics = []
-     TopicCount = Counter(" ".join(newdset.text).split()).most_common(15)
+     most_important_topics_with_freq = {}
+     TopicCount = Counter(" ".join(newdset.text).lower().split()).most_common(15)
      for topic_name in TopicCount:
           most_important_topics.append(topic_name[0])
+          most_important_topics_with_freq[topic_name[0]]=topic_name[1]
      print(most_important_topics)
      
      #LDA
@@ -109,21 +121,35 @@ while i<10:
      
      # Assigning Event Type
      event_type = ""
-     earthquake_found = any(elem in most_important_topics  for elem in ["earthquake", "earthquakes", "quake"]) 
-     fire_found = any(elem in most_important_topics  for elem in ["fire", "wildfire", "flames"])
-     cyclone_found = any(elem in most_important_topics  for elem in ["cyclone", "storm", "winds"])
+     earthquake_list = ["earthquake", "earthquakes", "quake"]
+     earthquake_found = any(elem in most_important_topics  for elem in earthquake_list) 
+     fire_list = ["fire", "wildfire", "flames"]
+     fire_found = any(elem in most_important_topics  for elem in fire_list)
+     cyclone_list = ["cyclone", "storm", "winds"]
+     cyclone_found = any(elem in most_important_topics  for elem in cyclone_list)
+     event_count = 0
      
      if earthquake_found:
-       event_type = "earthquake"
-     elif fire_found:
-       event_type = "fire"
-     elif cyclone_found:
+          event_type = "earthquake"
+          event_count = event_count + 1
+     if fire_found:
+          event_type = "fire"
+          event_count = event_count + 1
+     if cyclone_found:
           event_type = "cyclone"
-     else:
-          event_type="None"
-     #if(event_type=="None"):
-     #     return
-     #print(event_type)
+          event_count = event_count + 1
+     
+     if event_count == 0:
+          time.sleep(300)
+          continue
+     
+     if event_count > 1:
+          sorted_topic_list = sorted(most_important_topics_with_freq.items(), key = lambda kv:(kv[1], kv[0]))
+          for sorted_topic in reversed(sorted_topic_list):
+               if sorted_topic[0] in earthquake_list or sorted_topic[0] in fire_list or sorted_topic[0] in cyclone_list: 
+                    event_type = sorted_topic[0]
+                    break
+          
      
      # Semantic Analysis
      ground_truth_semantic = nlp("Earthquake")
@@ -177,19 +203,26 @@ while i<10:
      loc_freqs = []
      for el in event_loc_most:
           loc_names.append(el[0])
-          loc_freqs.append(str(el[1]))
+          loc_freqs.append(str(el[1]))     
+     if len(loc_names) < 3:
+          loc_name_i = len(loc_names)
+          while loc_name_i < 3:
+               loc_names.append("")
+               loc_freqs.append(0) 
+               loc_name_i = loc_name_i + 1
      
      # Parsing help tweets to be compatible for GraphQL     
      help_tweet_str = ''  
      for h_twt in list(help_tweets.values()):
-          help_tweet_str = "{orgStr}{{tweet:\\\"{rowTweet}\\\", location:\\\"{rowLoc}\\\", timestamp: \\\"{rowTime}\\\", status: \\\"1\\\" }},".format(orgStr=help_tweet_str, rowTweet=h_twt['tweet'], rowLoc=h_twt['location'], rowTime=datetime.strptime(h_twt['timestamp'], "%Y-%m-%d %H:%M:%S"))
+          help_tweet_str = "{orgStr}{{tweet:\\\"{rowTweet}\\\", location:\\\"{rowLoc}\\\", timestamp: \\\"{rowTime}\\\", status: \\\"1\\\" }},".format(orgStr=help_tweet_str, rowTweet=h_twt['tweet'], rowLoc=h_twt['location'], rowTime=datetime.strptime(h_twt['timestamp'], "%Y-%m-%dT%H:%M:%S.000Z"))
      #print(help_tweet_str[:-1])
 
      #Saving data to MongoDB
-     url = 'https://us-east-1.aws.realm.mongodb.com/api/client/v2.0/app/neptune_realm_1-uwjwy/graphql'
-     headers = {
+     graphql_url = 'https://us-east-1.aws.realm.mongodb.com/api/client/v2.0/app/neptune_realm_1-uwjwy/graphql'
+     graphql_headers = {
        'apiKey': 'tnpTbJUWMxGtVF5jHebxZRmjlI7Lxm45W0Gabc6diaVqs2IaVpzpm3Fg5gXzQu4A',
-       'Content-Type': 'application/json'
+       'Content-Type': 'application/json',
+       'charset':'utf-8'
      }
      #data = "\t{{\r\n\t  \t\"query\": \"mutation {{ updateOneCurrent_event( query: {{ event_type: \\\"{ev}\\\" }} set: {{  locations: [\\\"{loc2}\\\", \\\"{loc1}\\\", \\\"{loc0}\\\"], location_frequency: [\\\"{freq2}\\\", \\\"{freq1}\\\", \\\"{freq0}\\\"] }} ) {{ event_type locations location_frequency }} }}\",\r\n\t\t\"variables\": null\r\n\t}}".format(ev=event_type, loc2=loc_names[2], loc1=loc_names[1], loc0=loc_names[0], freq2=loc_freqs[2], freq1=loc_freqs[1], freq0=loc_freqs[0], hlp_twt_array=list(help_tweets.values()))
      data = "\t{{\r\n\t  \t\"query\": \"mutation {{ upsertOneCurrent_event( query: {{ event_type: \\\"{ev}\\\" }} data: {{  event_type: \\\"{ev}\\\", locations: [\\\"{loc2}\\\", \\\"{loc1}\\\", \\\"{loc0}\\\"], location_frequency: [\\\"{freq2}\\\", \\\"{freq1}\\\", \\\"{freq0}\\\"] }} ) {{ event_type locations location_frequency }} }}\",\r\n\t\t\"variables\": null\r\n\t}}".format(ev=event_type, loc2=loc_names[2], loc1=loc_names[1], loc0=loc_names[0], freq2=loc_freqs[2], freq1=loc_freqs[1], freq0=loc_freqs[0], hlp_twt_array=list(help_tweets.values()))
@@ -198,21 +231,22 @@ while i<10:
      response = requests.request(
        'POST',
        'https://us-east-1.aws.realm.mongodb.com/api/client/v2.0/app/neptune_realm_1-uwjwy/graphql',
-       data=data,
-       headers=headers,
+       data=data.encode('utf-8'),
+       headers=graphql_headers,
      )
      
      response2 = requests.request(
       'POST',
        'https://us-east-1.aws.realm.mongodb.com/api/client/v2.0/app/neptune_realm_1-uwjwy/graphql',
-       data=data2,
-       headers=headers,
+       data=data2.encode('utf-8'),
+       headers=graphql_headers,
      )
-
-     print("iteration {}".format(i))
-     i = i + 1     
      
-     time.sleep(300)
+
+     print("iteration {}".format(loop_i))
+     loop_i = loop_i + 1     
+     
+     time.sleep(60)
 
 
 #def main():
